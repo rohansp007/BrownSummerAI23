5e03da524eab0aefb4b5a57d17003a52ca399a5c
Rohan Parthipan <rohan.parthipan@gmail.com>
2023-07-02 14:18:12 -0400

    version 3

--- summer_ai_week1_project-main/social_network.py
+++ summer_ai_week1_project-main/social_network.py
@@ -1,12 +1,34 @@
-from  social_network_classes import SocialNetwork,Person
+from  social_network_classes import social_network,Person
 import social_network_ui
-
-ai_social_network = SocialNetwork()
 
 if __name__ == "__main__":
     print("########################################################")
     print("          Welcome to Summer AI Social Network")
     print("########################################################")
     last_menu = None
-    logging_in = social_network_ui.userLogin()
+while True:
     choice = social_network_ui.mainMenu()
+    if choice == '1':
+        social_network_ui.createAccount()
+    elif choice == '2':
+        login_bool = social_network_ui.userLogin()
+        if login_bool == True:
+            social_network_ui.manageAccountMenu()
+        else:
+            continue
+        manage_choice = social_network_ui.manageAccountMenu()
+        if manage_choice == '6':
+            continue
+        else:
+            if manage_choice == '1':
+                pass
+            if manage_choice == '2':
+                pass
+            if manage_choice == '3':
+                pass
+            if manage_choice == '4':
+                pass
+            if manage_choice == '5':
+                pass
+    elif choice == '3':
+        break
